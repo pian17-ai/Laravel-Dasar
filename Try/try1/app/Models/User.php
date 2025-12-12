8cfb66784f223bdd82467b98d3fc3fe915607426
pian17-ai <alviancahyopambudi@gmail.com>
2025-12-12 15:30:35 +0700

    create Project and Migration

---
+++ Try/try1/app/Models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Model
+{
+    use HasApiTokens;
+
+    protected $fillable = [
+        'username',
+        'email',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+}
