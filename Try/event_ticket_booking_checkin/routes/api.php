6bed75ef130ef0ecc1f0b7693db0c1b28917adf4
pian17-ai <alviancahyopambudi@gmail.com>
2025-12-28 17:55:09 +0700

    add new project and AUTH TIMES = 18m 31s

---
+++ Try/event_ticket_booking_checkin/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// auth
+Route::prefix('/v1/auth')->group(function() {
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
+    Route::middleware('auth:sanctum')->post('/logout', [UserController::class, 'logout']);
+});
