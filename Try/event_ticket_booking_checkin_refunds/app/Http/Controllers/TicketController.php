227f17cb28d41f8c0e8a9be94f82390fce00a384
pian17-ai <alviancahyopambudi@gmail.com>
2025-12-30 18:14:55 +0700

    CRUD and route Ticket | TIME = 15m 47s

--- Try/event_ticket_booking_checkin_refunds/app/Http/Controllers/TicketController.php
+++ Try/event_ticket_booking_checkin_refunds/app/Http/Controllers/TicketController.php
@@ -2,9 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    public function index() {
+        $tickets = Ticket::get();
+
+        return response()->json([
+            'message' => 'success get all tickets',
+            'data' => $tickets
+        ], 200);
+    }
+
+    public function show(Ticket $ticket) {
+        return response()->json([
+            'message' => 'success get ticket',
+            'data' => $ticket
+        ], 200);
+    }
+
+    public function store(Event $event, Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|integer',
+            'quota' => 'required|integer'
+        ]);
+
+        $ticket = Ticket::create([
+            'event_id' => $event->id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quota' => $request->quota
+        ]);
+
+        return response()->json([
+            'message' => 'ticket added',
+            'data' => $ticket
+        ], 201);
+    }
+
+    public function update(Request $request, Ticket $ticket) {
+        $validated = $request->validate([
+            'name' => 'required',
+            'price' => 'required|integer',
+            'quota' => 'required|integer'
+        ]);
+
+        $ticket->update($validated);
+
+        return response()->json([
+            'message' => 'event updated',
+            'data' => $ticket
+        ], 200);
+    }
+
+    public function destroy(Ticket $ticket) {
+        $ticket->delete();
+
+        return response()->json([
+            'message' => 'event deleted'
+        ], 200);
+    }
 }
--- Try/event_ticket_booking_checkin_refunds/routes/api.php
+++ Try/event_ticket_booking_checkin_refunds/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\TicketController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,10 @@ Route::middleware('auth:sanctum', 'admin')->group(function() {
 });
 
 //ticket
-// Route::middleware('auth:sanctum')->group(function() {
-
-// });
+Route::get('/tickets', [TicketController::class, 'index']);
+Route::get('/tickets/{ticket}', [TicketController::class, 'show']);
+Route::middleware('auth:sanctum', 'admin')->group(function() {
+    Route::post('/events/{event}/tickets', [TicketController::class, 'store']);
+    Route::put('/tickets/{ticket}', [TicketController::class, 'update']);
+    Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy']);
+});
