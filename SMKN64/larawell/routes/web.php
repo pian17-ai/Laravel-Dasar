deaa4648906d38e6c97f0e554c825bd177cc4039
pian17-ai <alviancahyopambudi@gmail.com>
2026-01-19 14:56:22 +0700

    Add Index Function

--- SMKN64/larawell/routes/web.php
+++ SMKN64/larawell/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+
+Route::resource('/products', ProductController::class);
 
 Route::get('/', function () {
     return view('welcome');
